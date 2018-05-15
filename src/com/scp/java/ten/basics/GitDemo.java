9decf7a87e21cdc98f4602f5ab46cca1ba6b52ce
yogymax@gmail.com <yogymax@gmail.com>
2018-05-15 09:28:16 +0530

    Both the changes

--- src/com/scp/java/ten/basics/GitDemo.java
+++ src/com/scp/java/ten/basics/GitDemo.java
@@ -5,7 +5,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("HI this is first chanege");
 		System.out.println("Change added by other dev");
-		
+		System.out.println("this is second change...");
 	}
 	
 }
