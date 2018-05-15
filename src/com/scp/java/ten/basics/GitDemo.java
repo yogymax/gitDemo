990f772407dcf981bfeb863d746419d05dbe18a5
yogymax <yogymax@gmail.com>
2018-05-15 09:22:40 +0530

    Update GitDemo.java

--- src/com/scp/java/ten/basics/GitDemo.java
+++ src/com/scp/java/ten/basics/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("HI this is first chanege");
+		System.out.println("Change added by other dev");
+		
 	}
 	
 }
